63c03e2a6390e2bf7ec413734a4a9a77d2c17135
LSY <vwb123123@naver.com>
2026-02-24 17:24:38 +0900

    상품목록 오류 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,27 +1,17 @@
 import axios from "axios";
-import useAuthStore from "../store/useAuthStore.ts";
 
 const BASE_URL = "/api";
-const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const httpClient = axios.create({
     baseURL: BASE_URL,
     headers: {
         "Content-Type": "application/json",
-        "X-client-key": API_KEY,
     },
 });
 
 httpClient.interceptors.request.use((config) => {
-    const token = useAuthStore.getState().token;
-
-    const isAuthRequest =
-        config.url?.includes("/auth/login") ||
-        config.url?.includes("/auth/register");
-
-    if (token && !isAuthRequest) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+    config.headers["x-client-key"] =
+        "b6J3jwUB5EPJ9bezM5m2teC2Y9LQXLmZshlPjvwiqHlHfjSea3boDKMCcPxE8xct";
 
     return config;
 });
--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,10 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
         proxy: {
-            "/api": {
+            "^/api/.*": {
                 target: "http://49.247.30.225:4104",
                 changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, "/api"),
             },
         },
     },
